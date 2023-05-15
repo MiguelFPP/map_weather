3735c3451cc6b2f2e0e18158327dc0b8e4c66631
Jhoan Stiven Montealegre Quitora <miguelferneyp@gmail.com>
2023-05-15 08:25:15 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,7 +73,7 @@
    
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">JVN</a>
+          <a class="navbar-brand" href="{{route('home')}}">JVN</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -192,13 +192,9 @@
   </script>
   
   <script>
-    /* ready function vanila js */
     document.addEventListener('DOMContentLoaded', function() {
-        /* evento click */
         document.getElementById('view-history').addEventListener('click', function() {
-            /* obtenemos el elemento */
             var element = document.querySelector('.list-history');
-            /* verify if have display none */
             if (element.style.display == 'block') {
                 element.style.display = 'none';
                 this.innerHTML = 'Ver historial';
